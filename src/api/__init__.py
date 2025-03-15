e70933846198307133d64284bced1c4fd287d3c9
popovtt <108386075+popovtt@users.noreply.github.com>
2025-03-15 22:36:04 +0200

    Added analysis to api/v1

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from .notes.views import router as notes_router
 from .notes_versions.views import router as versions_router
+from .analysis.views import router as analysis_router
 
 
 router = APIRouter()
 router.include_router(router=notes_router, prefix="/notes")
 router.include_router(router=versions_router, prefix="/notes-versions")
+router.include_router(router=analysis_router, prefix="/analysis")
