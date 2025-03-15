0924e55ac57fe08084d922f91cf86e996177bf4b
popovtt <108386075+popovtt@users.noreply.github.com>
2025-03-15 11:57:29 +0200

    Added get_notes method.

--- src/api/notes/views.py
+++ src/api/notes/views.py
@@ -1,7 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.models import db_helper
+from ..notes import crud
 
 
 router = APIRouter(tags=["Notes"])
+
+
+@router.get("/notes")
+async def get_notes(session: AsyncSession = Depends(db_helper.session_dependency)):
+    return await crud.read_notes(session=session)
 
 
 @router.get("/hello")
