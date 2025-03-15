d5a707e15d8daca3339d57b0b73f8f31bc0e4970
popovtt <108386075+popovtt@users.noreply.github.com>
2025-03-15 18:46:51 +0200

    Added notes-versions router.

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .notes.views import router as notes_router
+from .notes_versions.views import router as versions_router
 
 
 router = APIRouter()
 router.include_router(router=notes_router, prefix="/notes")
+router.include_router(router=versions_router, prefix="/notes-versions")
