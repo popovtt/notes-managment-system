2cbee4bd765536c377e7a2758bc9773744c73133
popovtt <108386075+popovtt@users.noreply.github.com>
2025-03-15 11:59:24 +0200

    Added Note model to src.models.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,11 @@
 __all__ = (
     "Base",
+    "Note",
     "created_at",
     "updated_at",
-    "Note",
+    "db_helper",
 )
 
+from ..database import db_helper
 from src.models.base import Base, created_at, updated_at
 from src.models.note import Note
