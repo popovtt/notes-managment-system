f5700dc8a85b8b9a3e533dd36b1e54a0bdf3406a
popovtt <108386075+popovtt@users.noreply.github.com>
2025-03-15 11:01:20 +0200

    Added Base.metadata to alembic configuration.

--- alembic/env.py
+++ alembic/env.py
@@ -19,7 +19,8 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from src.config import settings
-target_metadata = None
+from src.models import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
