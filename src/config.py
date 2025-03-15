28b3ab1368e1ffc1994605e55d3ee07c5298d6d5
popovtt <108386075+popovtt@users.noreply.github.com>
2025-03-15 11:59:02 +0200

    Added env_path.

--- src/config.py
+++ src/config.py
@@ -1,9 +1,13 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+env_file = f"{Path(__file__).parent.parent}/.env"
+
+
 class DBSettings(BaseSettings):
-    DB_USER: str
     DB_NAME: str
+    DB_USER: str
     DB_PASS: str
     DB_HOST: str
     DB_PORT: str
@@ -12,7 +16,7 @@ class DBSettings(BaseSettings):
     def DATABASE_URL_ASYNCPG(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=env_file)
 
 
 class Settings(BaseSettings):
