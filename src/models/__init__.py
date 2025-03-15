7ea62856dfd549c6ca838314f109f7343ac7479a
popovtt <108386075+popovtt@users.noreply.github.com>
2025-03-15 17:04:14 +0200

    Added NoteVersion model.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,8 +4,10 @@ __all__ = (
     "created_at",
     "updated_at",
     "db_helper",
+    "NoteVersion",
 )
 
 from ..database import db_helper
 from src.models.base import Base, created_at, updated_at
 from src.models.note import Note
+from src.models.note_version import NoteVersion
