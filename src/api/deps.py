d04739e424c2b3bbb632bae9da278bf51d7ef9a9
popovtt <108386075+popovtt@users.noreply.github.com>
2025-03-16 12:14:14 +0200

    notes_list functionu used in ai`s & analysis`s CRUDs.

---
+++ src/api/deps.py
@@ -0,0 +1,18 @@
+from fastapi import HTTPException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.models import Note
+
+
+async def notes_list(session: AsyncSession):
+    stmt = select(Note.title)
+    result = await session.execute(stmt)
+    notes = [row[0] for row in result.all()]
+
+    if not notes:
+        raise HTTPException(
+            status_code=404,
+            detail="No notes found"
+        )
+
+    return notes
