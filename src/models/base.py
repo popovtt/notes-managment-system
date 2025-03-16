ee9c20bd1d59fc89e162012f57c3a4b5e5c17f8a
popovtt <108386075+popovtt@users.noreply.github.com>
2025-03-16 12:18:03 +0200

    Added created_at & updated_at fields to Base model.

--- src/models/base.py
+++ src/models/base.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from typing import Annotated
-
 from sqlalchemy import func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
@@ -15,6 +14,12 @@ updated_at = Annotated[datetime, mapped_column(
 
 class Base(DeclarativeBase):
     id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now,server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        default=datetime.now,
+        server_default=func.now(),
+        onupdate=datetime.now,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
