b27d87d27e2a6343e66fdcee208a2d47ad2d69ff
popovtt <108386075+popovtt@users.noreply.github.com>
2025-03-17 11:02:06 +0200

    Added response_model.

--- src/api/notes/views.py
+++ src/api/notes/views.py
@@ -1,20 +1,22 @@
+from typing import List
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.models import db_helper, Note
 from . import crud
 from .deps import note_by_id
-from .schemas import NoteCreateSchema,NoteUpdateSchema
+from .schemas import NoteCreateSchema, NoteUpdateSchema, NoteSchema
 
 
 router = APIRouter(tags=["Notes"])
 
 
-@router.get("/")
+@router.get("/", response_model=List[NoteSchema], summary="Get All Notes")
 async def get_notes(session: AsyncSession = Depends(db_helper.session_dependency)):
     return await crud.read_notes(session=session)
 
 
-@router.get("/{note_id}", summary="Get Note by ID")
+@router.get("/{note_id}", response_model=NoteSchema, summary="Get Note by ID")
 async def get_note_by_id(
         note_id: int,
         session: AsyncSession = Depends(db_helper.session_dependency)
@@ -22,7 +24,7 @@ async def get_note_by_id(
     return await crud.read_note_by_id(note_id=note_id, session=session)
 
 
-@router.get("/title/")
+@router.get("/title/", response_model=NoteSchema, summary="Get Note by Title")
 async def get_note_by_title(
         note_title: str,
         session: AsyncSession = Depends(db_helper.session_dependency)
@@ -30,7 +32,7 @@ async def get_note_by_title(
     return await crud.read_note_by_title(session=session, title=note_title)
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=NoteSchema, status_code=status.HTTP_201_CREATED, summary="Create Note")
 async def post_note(
         note_in: NoteCreateSchema,
         session: AsyncSession = Depends(db_helper.session_dependency),
@@ -38,7 +40,7 @@ async def post_note(
     return await crud.create_note(note_in=note_in, session=session)
 
 
-@router.patch("/")
+@router.patch("/", response_model=NoteSchema, summary="Update Note")
 async def patch_note(
         note_in: NoteUpdateSchema,
         note: Note = Depends(note_by_id),
